ab4b33fa5727935b6efe88257af85c3631760d9e
Nate <natemoss02@gmail.com>
2018-07-26 16:36:20 -0500

    Added Parallax Background

---
+++ Assets/Parallax.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    public GameObject player;
+    public Rigidbody2D rb;
+    public float division;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(player.GetComponent<Rigidbody2D>().velocity.x / division, player.GetComponent<Rigidbody2D>().velocity.y / division);
+	}
+}
